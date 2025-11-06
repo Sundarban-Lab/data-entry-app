0339f1610db7ee94b0a13ca9f9395781677b9680
Anik Chowdhury <bloggeranik1986@gmail.com>
2025-11-07 02:11:48 +0600

    feat: customizable fields UI and external link handling
    - Centered header/footer; inputs now full-width
    - Field manager initialization stabilized
    - Secure external link open via IPC (preload/main)
    - Minor styling/layout improvements

--- preload.js
+++ preload.js
@@ -1,4 +1,6 @@
-const { contextBridge, ipcRenderer, shell } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
+
+console.log('Preload script running, exposing API...');
 
 contextBridge.exposeInMainWorld('api', {
   getData: () => ipcRenderer.invoke('get-data'),
@@ -6,5 +8,11 @@ contextBridge.exposeInMainWorld('api', {
   updateData: (record) => ipcRenderer.invoke('update-data', record),
   deleteData: (id) => ipcRenderer.invoke('delete-data', id),
   exportExcel: () => ipcRenderer.invoke('export-excel'),
-  openExternal: (url) => shell.openExternal(url)
+  openExternal: (url) => {
+    console.log('openExternal called with url:', url);
+    // Call main process to open external link
+    return ipcRenderer.invoke('open-external', url);
+  }
 });
+
+console.log('API exposed successfully');
